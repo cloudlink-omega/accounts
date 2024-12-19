dbef9a91232a36318008a2727e77fa4f52392cbb
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2024-12-19 15:54:32 -0500

    WIP: account recovery page

---
+++ pkg/pages/recovery.go
@@ -0,0 +1,17 @@
+package pages
+
+import (
+	"github.com/cloudlink-omega/accounts/pkg/sanitizer"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (p *Pages) RecoveryLanding(c *fiber.Ctx) error {
+	data := map[string]interface{}{
+		"BaseURL":        p.RouterPath,
+		"ServerName":     p.ServerName,
+		"PrimaryWebsite": p.PrimaryWebsite,
+		"Redirect":       sanitizer.Sanitized(c, c.Query("redirect")),
+	}
+	c.Context().SetContentType("text/html; charset=utf-8")
+	return c.Render("views/recovery", data, "views/layout")
+}
