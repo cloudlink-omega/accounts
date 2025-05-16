863b3ac84ec168bf95c30c92000ba7b17d9c569a
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2025-05-16 16:40:14 -0400

    Move migrations to storage repo

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,24 +1,10 @@
 package database
 
 import (
-	"github.com/cloudlink-omega/storage/pkg/types"
 	"gorm.io/gorm"
 )
 
 type Database struct {
 	DB           *gorm.DB // The database connection.
 	ServerSecret string   // A 32-byte (256-bit) secret used for encryption/decryption.
-}
-
-func (d *Database) RunMigrations() error {
-	return d.DB.AutoMigrate(
-		&types.User{},
-		&types.Verification{},
-		&types.RecoveryCode{},
-		&types.UserGoogle{},
-		&types.UserDiscord{},
-		&types.UserGitHub{},
-		&types.UserTOTP{},
-		&types.UserSession{},
-	)
 }
